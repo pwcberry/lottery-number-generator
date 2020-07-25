4ef09d65bb481625a9eb1099e50f07b91e16b3f2
Peter Berry <pwcberry@gmail.com>
2020-07-25 16:13:55 +1000

    Use gh-pages CLI

--- scripts/publish.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const ghpages = require("gh-pages");
-
-ghpages.publish(path.resolve(__dirname, "../src"), err => {
-    if (err) {
-        console.error(err);
-    }
-});
