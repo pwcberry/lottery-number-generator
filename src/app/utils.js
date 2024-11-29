2053a9ad8de50b17afc9c6c138593e8a0847fbf4
Peter Berry <pwcberry@gmail.com>
2024-11-30 10:32:45 +1100

    Define function "generateGames"

--- src/app/utils.js
+++ src/app/utils.js
@@ -1,5 +1,5 @@
 export function* range(min, max) {
-    let i =  min;
+    let i = min;
     while (i <= max) {
         yield i;
         i += 1;
@@ -10,14 +10,37 @@ export function hash(list) {
     return list.reduce((acc, n) => acc | 2n ** BigInt(n), 0n);
 }
 
-// export function* generateGames(selectedNumbers, limit) {
-//     let pool = new Set();
-//     let i = limit;
-//
-//     while (i > 0) {
-//         // generate random numbers from selectedNumbers
-//         // hash this list
-//         // check if hash is in pool
-//         // if not in pool, add to pool and yield list
-//     }
-// }
+function getRandomInt(max, min = 0) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function generateGames(setOfNumbers, maxGames = 12, maxPicksPerGame = 6) {
+    const pool = new Map();
+    const randomIndexLimit = setOfNumbers.length - 1;
+    let gameCounter = maxGames;
+
+    while (gameCounter > 0) {
+        const usedIndexes = [];
+
+        // maxPicksPerGame must be larger than setOfNumbers.length
+        let pickCounter = maxPicksPerGame;
+        const gamePicks = [];
+
+        while (pickCounter > 0) {
+            const index = getRandomInt(randomIndexLimit);
+            if (!usedIndexes.includes(index)) {
+                gamePicks.push(setOfNumbers[index]);
+                usedIndexes.push(index);
+                pickCounter -= 1;
+            }
+        }
+
+        const gameHash = hash(gamePicks);
+        if (!pool.has(gameHash)) {
+            pool.set(gameHash, gamePicks);
+            gameCounter -= 1;
+        }
+    }
+
+    return Array.from(pool.values());
+}
