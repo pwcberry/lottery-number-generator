9d7e318ef168307d1ea739b11edfe532187ad806
Peter Berry <pwcberry@gmail.com>
2025-08-31 22:30:17 +1000

    Add rule to prevent a game resembling a previous game

--- src/app/utils.js
+++ src/app/utils.js
@@ -14,10 +14,33 @@ function getRandomInt(max, min = 0) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+/**
+ *
+ * @param current {BigInt} The current set of numbers represented as a hash
+ * @param previous {BigInt} The previous set of numbers represented as a hash
+ * @param threshold {Number} The maximum number of matches to make the current set valid
+ * @param maxSize {Number} The maximum range of numbers for a game
+ */
+function doesGameResemblePrevious(current, previous, threshold = 3, maxSize = 63) {
+    const matches = [];
+
+    for (let i = 0; i < maxSize; i += 1) {
+        const n = 2n ** BigInt(i);
+        const c = current & n;
+        const p = previous & n;
+        if (c === n && p === n) {
+            matches.push(i);
+        }
+    }
+
+    return matches.length > threshold;
+}
+
 export function generateGames(setOfNumbers, maxGames = 12, maxPicksPerGame = 6) {
     const pool = new Map();
     const randomIndexLimit = setOfNumbers.length - 1;
     let gameCounter = maxGames;
+    let previousGameHash = 0n;
 
     while (gameCounter > 0) {
         const usedIndexes = [];
@@ -36,10 +59,11 @@ export function generateGames(setOfNumbers, maxGames = 12, maxPicksPerGame = 6) 
         }
 
         const gameHash = hash(gamePicks);
-        if (!pool.has(gameHash)) {
+        if (!doesGameResemblePrevious(gameHash, previousGameHash, 2) && !pool.has(gameHash)) {
             gamePicks.sort((a, b) => a - b);
             pool.set(gameHash, gamePicks);
             gameCounter -= 1;
+            previousGameHash = gameHash;
         }
     }
 
