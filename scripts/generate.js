d85cf0e6a1e4b41f09d80af0aab98cc5d6682c6c
Peter Berry <pwcberry@gmail.com>
2026-02-13 10:55:19 +1100

    Update setOfNumbers in generate.js

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,7 +1,7 @@
 import { generateGames } from "../src/app/utils.js";
 
 function main() {
-    const setOfNumbers = [1, 2, 4, 5, 6, 9, 12, 13, 17, 19, 22, 23, 25, 27, 30, 36, 41, 42, 44];
+    const setOfNumbers = [1, 2, 4, 5, 6, 9, 12, 13, 16, 19, 22, 23, 25, 27, 30, 36, 40, 42, 44];
     const result = generateGames(setOfNumbers);
 
     result.forEach((game, index) => {
